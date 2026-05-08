cc5629367e53134f35f57addac7ab6078ce4d416
izzooooooo <izzetqasimov2007@gmail.com>
2026-05-08 22:34:34 +0400

    Fix indentation and add newline at end of file

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
+"""Redis basic cache."""
 import redis
 from typing import Union
 from uuid import uuid4
 
+
 class Cache:
+    """Cache class."""
+
     def __init__(self):
+        """Initialize Redis client and flush the DB."""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
+        """Store data in Redis and return the key."""
         key = str(uuid4())
         self._redis.set(key, data)
         return key
