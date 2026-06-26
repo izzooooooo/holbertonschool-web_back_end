f30123392494cfa73ea42f7793dedd34238713cb
Izzat Gasimov <izzetqasimov2007@gmail.com>
2026-06-26 23:15:17 +0400

    Update 4-app.py

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -1,43 +1,38 @@
 #!/usr/bin/env python3
-""" Route module for the API - Force locale with URL parameter """
+"""Route module for the API - Force locale with URL parameter"""
 from flask import Flask, request, render_template
 from flask_babel import Babel
 from os import getenv
 
-app = Flask(__name__)
-babel = Babel(app)
-
 
 class Config(object):
-    """ Setup - Babel configuration """
+    """Setup - Babel configuration"""
     LANGUAGES = ['en', 'fr']
-    # these are the inherent defaults just btw
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
-# set the above class object as the configuration for the app
-app.config.from_object('4-app.Config')
+app = Flask(__name__)
+app.config.from_object(Config)  # ← string yox, birbaşa class
+
+
+def get_locale() -> str:
+    """Determines best match for supported languages"""
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+babel = Babel(app, locale_selector=get_locale)  # ← yeni API
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index() -> str:
-    """ GET /
+    """GET /
     Return: 4-index.html
     """
     return render_template('4-index.html')
-
-
-@babel.localeselector
-def get_locale() -> str:
-    """ Determines best match for supported languages """
-    # check if there is a locale parameter/query string
-    if request.args.get('locale'):
-        locale = request.args.get('locale')
-        if locale in app.config['LANGUAGES']:
-            return locale
-    else:
-        return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 if __name__ == "__main__":
