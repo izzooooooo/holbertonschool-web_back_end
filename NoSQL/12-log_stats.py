6d397f4263b59e152803fed6a8995272827b8cd6
Izzat Gasimov <izzetqasimov2007@gmail.com>
2026-07-05 23:22:30 +0400

    Refactor log_stats.py to use nginx_collection

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python3
-"""Module to provide stats about Nginx logs stored in MongoDB."""
+"""Provides some stats about Nginx logs stored in MongoDB"""
 from pymongo import MongoClient
 
 
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
-    collection = client.logs.nginx
+    nginx_collection = client.logs.nginx
 
-    total = collection.count_documents({})
-    print("{} logs".format(total))
+    total_logs = nginx_collection.count_documents({})
+    print("{} logs".format(total_logs))
 
     print("Methods:")
-    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
-        count = collection.count_documents({"method": method})
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        count = nginx_collection.count_documents({"method": method})
         print("\tmethod {}: {}".format(method, count))
 
-    status_check = collection.count_documents(
-        {"method": "GET", "path": "/status"}
-    )
+    status_check = nginx_collection.count_documents(
+        {"method": "GET", "path": "/status"})
     print("{} status check".format(status_check))
