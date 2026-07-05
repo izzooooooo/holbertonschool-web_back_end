892b0e62395258e052f2707c946f0a93ee1e630c
Izzat Gasimov <izzetqasimov2007@gmail.com>
2026-07-06 00:15:27 +0400

    Update 3-glam_rock.sql

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,5 @@
--- Task 3: Old school bandlists - all bands with Glam as their main style
--- ranked by their longevity
-SELECT DISTINCT `band_name`,
-                IFNULL(`split`, 2020) - `formed` as `lifespan`
-    FROM `metal_bands` WHERE FIND_IN_SET('Glam rock', style)
-    ORDER BY `lifespan` DESC;
+-- Lists all bands with Glam rock as their main style, ranked by their longevity
+SELECT band_name, (IFNULL(split, 2024) - formed) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
